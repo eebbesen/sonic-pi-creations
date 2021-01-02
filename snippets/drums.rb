45c6fca15129ce09e229a651ac89202bb13fe28f
eebbesen <eebbesen.git@gmail.com>
2021-01-02 14:43:01 -0600

    more drum beats

--- snippets/drums.rb
+++ snippets/drums.rb
@@ -1,3 +1,4 @@
+# base sound combos
 def snare
   with_fx :reverb do
     sample :drum_snare_hard, pan: -0.3
@@ -11,14 +12,25 @@ def kick
   end
 end
 
-def hh
+# doubles the hi-hat if sleep time is passed in
+def hh(sleep_time = nil)
   sample :drum_cymbal_pedal, pan: -0.8
+  if sleep_time
+    sleep sleep_time
+    hh
+  end
 end
 
 # standard hi-hat / kick / snare
 # can double hi-hat with @dhh set
 # sleep_time will be halved when hi-hat is doubled
-def explicit_drums_4_4(sleep_time)
+
+# examples
+# live_loop :ede do
+#   @dhh = false
+#   drums_4_4 1
+# end
+def drums_4_4(sleep_time)
   hhs = @dhh ? sleep_time / 2.0 : nil
 
   kick
@@ -42,4 +54,46 @@ def explicit_drums_4_4(sleep_time)
   else
     sleep sleep_time
   end
+end
+
+# swing gives a bit of swing
+# if @dhh is true and swing is false you get a beat in 5
+def drums_3_4(sleep_time, swing = false)
+
+  hhs = @dhh || swing ? sleep_time / 2.0 : nil
+
+  kick
+  hh
+
+  if hhs
+    sleep hhs
+    hh
+    sleep hhs
+  else
+    sleep sleep_time
+  end
+
+  if hhs
+    sleep hhs if swing
+    hh
+    sleep hhs
+  else
+    sleep sleep_time
+  end
+
+  snare
+  hh
+
+  if hhs
+    sleep hhs
+    hh
+    sleep hhs
+  else
+    sleep sleep_time
+  end
+end
+
+def drums_5_4(sleep_time)
+  @dhh = true
+  drums_3_4
 end
